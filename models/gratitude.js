7e5e0b3609772b1bea486423a86db50201ec919c
suziestephen <suzietstephen@gmail.com>
2021-05-03 21:08:21 +1000

    for gratitude table initial file modelsa

---
+++ models/gratitude.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+    
+    const Entries = sequelize.define('Entries', {
+      
+      habitName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1],
+        },
+      },  
+
+
+    gratefulFor: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1],
+    }, 
+    
+  },
+
+},
+  
+  
+  {timestamps: false});
+  
+    return Entries;
+  };
+  
+  
+    
+   
+  
