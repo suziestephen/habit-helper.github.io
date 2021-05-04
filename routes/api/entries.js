d6be1c2a3fa5be5fffa9229cca869f25a30d58ed
suziestephen <suzietstephen@gmail.com>
2021-05-04 21:41:55 +1000

    post return for testing

--- routes/api/entries.js
+++ routes/api/entries.js
@@ -20,29 +20,30 @@ router.route("/gratitude")
 
     // CHECK GET route for getting the exercise entries
     router.route("/exercise")
-
     .get((req, res) => {
         db.Exercise.findAll({}).then((dbExercise) => res.json(dbExercise));
     })
 
 
 //     // POST route for saving a new gratitude entry
-//     .post((req, res) => {
-//         console.log(req.body);
-//         db.Gratitude.create({
-//             gratefulFul: req.body.gratefulFor,
-//             date: req.body.date,
-//         }).then((dbGratitude) => res.json(dbGratitude));
-//     })
+    router.route("/gratitude")
+    .post((req, res) => {
+        console.log(req.body);
+        db.Gratitude.create({
+            gratefulFul: req.body.sunshine,
+            date: req.body.date,
+        }).then((dbGratitude) => res.json(dbGratitude));
+    })
 
 //     // PUT route for updating gratitude entries 
-//     .put((req, res) => {
-//         db.Gratitude.update(req.body, {
-//             where: {
-//                 id: req.body.id,
-//             },
-//         }).then((dbGratitude) => res.json(dbGratitude));
-//     });
+    router.route("/gratitude")
+    .put((req, res) => {
+        db.Gratitude.update(req.body, {
+            where: {
+                id: req.body.id,
+            },
+        }).then((dbGratitude) => res.json(dbGratitude));
+    });
 
 // // need for reading and exercise
 
