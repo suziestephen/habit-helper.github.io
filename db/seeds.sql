d851e6a1ff8acf319c3eabee82be64ee7e52b6f8
suziestephen <suzietstephen@gmail.com>
2021-05-04 20:39:24 +1000

    adding seed data

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO reading (book, pages, dayOf) VALUES ('The Great Gatbsy', 100, '21-7-8');
+
+INSERT INTO reading (book, pages, dayOf) VALUES ('The Great Gatbsy', 50, '21-7-9');
+
+INSERT INTO reading (book, pages, dayOf) VALUES ('The Great Gatbsy', 27, '21-7-7');
+
+INSERT INTO gratitude (gratefulFor, dayOf) VALUES ('Sunshine', '21-7-15');
