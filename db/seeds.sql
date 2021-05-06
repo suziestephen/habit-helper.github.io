361b08a05818380bb7096ea40127ef82c11a1b8b
suziestephen <suzietstephen@gmail.com>
2021-05-07 08:36:35 +1000

    adding seed data for exercise table

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,6 @@ INSERT INTO reading (book, pages, dayOf) VALUES ('The Great Gatbsy', 50, '21-7-9
 INSERT INTO reading (book, pages, dayOf) VALUES ('The Great Gatbsy', 27, '21-7-7');
 
 INSERT INTO gratitude (gratefulFor, dayOf) VALUES ('Sunshine', '21-7-15');
+
+INSERT INTO exercise (exercise, located, dayOf) VALUES ('Yoga', 'Outside', '21-7-8');
+
