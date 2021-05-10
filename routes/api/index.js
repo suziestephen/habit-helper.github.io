78a5c771380710702e9d1fad0d6a8c4ee57108a5
suziestephen <suzietstephen@gmail.com>
2021-05-10 20:50:09 +1000

    adding user to index file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const entriesRoutes = require("./entries");
+const userRoutes = require("./user");
 
 // Habit routes
 router.use("/entries", entriesRoutes);
+router.use("/user", userRoutes);
 
 module.exports = router;
 
