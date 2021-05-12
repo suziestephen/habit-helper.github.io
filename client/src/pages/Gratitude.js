3f3cc70c2e71346e92a5691fe5769dcc37399d25
suziestephen <suzietstephen@gmail.com>
2021-05-12 15:55:29 +1000

    new form

--- client/src/pages/Gratitude.js
+++ client/src/pages/Gratitude.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import { Row, Container } from "../components/Grid";
 import CreateGratitudeForm from "../components/CreateGratefulForm/index";
 
-const Gratitude = () => {
-  return (
-    <Container fluid>
-      <Row>
-          <CreateGratitudeForm />
-      </Row>
-    </Container>
-  );
-};
+class Gratitude extends Component {
+
+    state = {
+        fields: {}
+    };
+
+    onSubmit = fields => {
+        this.setState({ fields });
+    }
+
+    render() {
+        return (
+            <div className="Gratitude">
+            <Container fluid>
+            <Row>
+                <CreateGratitudeForm onSubmit={fields => this.onSubmit(fields)} />
+            </Row>
+            </Container>
+            </div>
+        );
+    }
+}
 
 export default Gratitude;
