c393f4250157cafd250c6b60235f2846f973d0c1
suziestephen <suzietstephen@gmail.com>
2021-05-12 11:55:32 +1000

    build install and fixing server file

---
+++ client/build/precache-manifest.12c93b1b6e70c44b809de83071533fd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fdb5ac005762740f537",
+    "url": "/static/css/main.b9571721.chunk.css"
+  },
+  {
+    "revision": "2fdb5ac005762740f537",
+    "url": "/static/js/main.c5e9e9de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bad95fac7e25c5fc7e98",
+    "url": "/static/js/2.95823d20.chunk.js"
+  },
+  {
+    "revision": "caaa8b15f6138b6828ef6afd86870b45",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,61 +1,35 @@
-const express = require('express');
-const session = require("express-session");
+const express = require("express");
 const path = require("path");
 
-//Passport 
-const passport = require("./config/passport");
-
-// Sets up the Express App
-const app = express();
-const PORT = process.env.PORT || 4000;
-const routes = require("./routes");
-
-
-
-// Requiring our models for syncing
+const PORT = process.env.PORT || 4001;
 const db = require('./models');
 
 
-
-// Sets up the Express app to handle data parsing
+const app = express();
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // // Serve up static assets (usually on heroku)
 // if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
-//   }
+//   app.use(express.static("client/build"));
+// }
 
-app.use(
-  session({ secret: "heroku", resave: true, saveUninitialized: true, cookie: {httpOnly: false}  })
-);
-
-
-// Initialize middleware, intialize passport
-app.use(passport.initialize());
-// Initialize middleware to alter the request object and deserialize "user" session ID from the request into a proper user object
-app.use(passport.session());
-
-
-
-// Static directory
-app.use(express.static('public'));
-
-// Routes
+//Routes
+const routes = require("./routes");
+// Add routes, both API and view
 app.use(routes);
 
-// REACT 
-// // Send every request to the React app
-// // Define any API routes before this runs
-// app.get("*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-//   });
-  
 
+// REACT 
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Syncing our sequelize models and then starting our Express app
-
-
 db.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Server listening on: http://localhost:${PORT}`));
 });
