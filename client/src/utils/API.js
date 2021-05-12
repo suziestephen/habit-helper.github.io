55f822eb1f4a0d009950fcf2fb405743c183c85a
suziestephen <suzietstephen@gmail.com>
2021-05-12 13:48:09 +1000

    fixing route path

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,21 @@
 import axios from "axios";
+const site = "http://localhost:3001";
 
 export default {
   // Gets all gratitude entries
   getGratitude: function() {
-    return axios.get("http://localhost:3001/api/gratitude");
+    return axios.get(site + "/api/entries/gratitude");
   },
   // Gets the gratitude entries with the given id
   getGratitude: function(id) {
-    return axios.get("http://localhost:3001/api/gratitude"+ id);
+    return axios.get(site + "api/entries/gratitude"+ id);
   },
   // Deletes the gratitude entries with the given id
   deleteGratitude: function(id) {
-    return axios.delete("http://localhost:3001/api/gratitude" + id);
+    return axios.delete(site + "api/entries/gratitude" + id);
   },
   // Saves a gratitude entries to the database
   saveGratitude: function(gratitudeData) {
-    return axios.post("http://localhost:3001/api/gratitude", gratitudeData);
+    return axios.post(site + "api/entries/gratitude", gratitudeData);
   }
 };
