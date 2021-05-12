2c49bde8b505f96b256cb45d06896110c841a7be
suziestephen <suzietstephen@gmail.com>
2021-05-12 15:55:39 +1000

    new form

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,10 @@ import NavBar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header.js";
 import Wrapper from "./components/Wrapper/Wrapper.js";
 import Footer from "./components/Footer/Footer";
-import Homepage from "./pages/Homepage";
+import Gratitude from "./pages/Gratitude";
+// import Exercise from "./pages/Exercise";
 // import Detail from "./pages/Detail";
-import { StoreProvider } from "./utils/GlobalState";
+// import { StoreProvider } from "./utils/GlobalState";
 // import FavoritesList from "./pages/FavoritesList";
 
 function App() {
@@ -21,17 +22,16 @@ function App() {
     <Router>
       <Wrapper>
       <div>
-        <StoreProvider>
           <NavBar />
           <Header />
           <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route exact path="/home" component={Homepage} />
+            <Route exact path="/" component={Gratitude} />
+            {/* <Route exact path="/" component={Exercise} /> */}
+            {/* <Route exact path="/home" component={Homepage} /> */}
             {/* <Route exact path="/favorites" component={FavoritesList} />
             <Route exact path="/posts/:id" component={Detail} /> */}
           </Switch>
           <Footer />
-        </StoreProvider>
       </div>
       </Wrapper>
     </Router>
