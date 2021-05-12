d564630a2cda8d7fc5b73ac164b3a133f0251e4b
suziestephen <suzietstephen@gmail.com>
2021-05-12 16:24:24 +1000

    fixing form component to save to db

--- client/src/components/CreateGratefulForm/index.js
+++ client/src/components/CreateGratefulForm/index.js
@@ -10,7 +10,7 @@ class CreateGratitudeForm extends Component {
       dayOf: "",
   }
 
-  change = (e) => {
+  change = e => {
     this.setState({
       [e.target.name]: e.target.value
     });
@@ -21,7 +21,7 @@ class CreateGratitudeForm extends Component {
     API.saveGratitude({
       gratefulFor: this.state.gratefulFor,
       dayOf: this.state.dayOf,
-    })
+    });
   };
 
   render() {
@@ -40,7 +40,7 @@ class CreateGratitudeForm extends Component {
         value={this.state.dayOf} 
         onChange={e => this.change(e)}
         />
-      <button onClick={e => this.onSubmit()}>Submit</button>
+      <button onClick={e => this.onSubmit(e)}>Submit</button>
 
   
       </form>
@@ -57,83 +57,3 @@ export default CreateGratitudeForm;
 
 
 
-
-
-
-//   //set state of saved books (variable: function)
-//   componentDidMount() {
-//       API.saveGratitude()
-//           .then(saveGratitude => this.setState({ saveGratitude: saveGratitude }))
-//           .catch(err => console.error(err));
-//   }
-
-
-//     handleSubmit = gratitude => {
-//           API.saveGratitude(gratitude)
-//               .then(saveGratitude => this.setState({ saveGratitude: this.state.saveGratitude.concat([saveGratitude]) }))
-//               .catch(err => console.error(err));
-//       }
-//     }
-
-//   render() 
-//   return (
-//     <div>
-//       <div className="header">
-//       </div>
-//       <h1>What are you grateful for today</h1>
-
-//     </div>
-//   )
-//   };
-
-
-// export default CreateGratitudeForm;
-
-
-
-
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   dispatch({ type: LOADING });
-  //   API.saveGratitude({
-  //     gratefulFor: gratefulForRef.current.value,
-  //     dayOf: dayOfRef.current.value,
-  //   })
-  //     .then((result) => {
-  //       dispatch({
-  //         type: ADD_GRATITUDE,
-  //         post: result.data,
-  //       });
-  //     })
-  //     .catch((err) => console.log(err));
-
-  //   gratefulForRef.current.value = '';
-  //   dayOfRef.current.value = '';
-  // };
-
-
-//   <form className="form-group mt-5 mb-5" onSubmit={handleSubmit}>
-//   <label htmlFor="gratefulFor">Grateful For:</label>
-//   <input
-//     className="form-control mb-5"
-//     required
-//     // ref={gratefulForRef}
-//     id="gratefulFor"
-//     placeholder="grateful for"
-//   />
-//   <label htmlFor="body">Date:</label>
-//   <textarea
-//     className="form-control mb-5"
-//     required
-//     // ref={dateRef}
-//     id="dayOf"
-//     placeholder="YYYY/MM/DD"
-//   />
-//   <button
-//     className="btn btn-success mt-3 mb-5"
-//     type="submit"
-//   >
-//     Save Entry
-//   </button>
-// </form>
