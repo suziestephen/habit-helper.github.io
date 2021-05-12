95075f4e4f68385aa3f1b013d4ce06a774138c9b
suziestephen <suzietstephen@gmail.com>
2021-05-12 11:54:46 +1000

    fix app for form setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,42 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+
+// import Entry from "./pages/Entry";
+// import Habits from "./pages/Habits";
+// import SignupFrom from "./pages/Signup";
+
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header.js";
 import Wrapper from "./components/Wrapper/Wrapper.js";
 import Footer from "./components/Footer/Footer";
+import Homepage from "./pages/Homepage";
+// import Detail from "./pages/Detail";
+import { StoreProvider } from "./utils/GlobalState";
+// import FavoritesList from "./pages/FavoritesList";
 
-// import Entry from "./pages/Entry";
-// import Habits from "./pages/Habits";
-import SignupFrom from "./pages/Signup";
-
-
-class App extends Component {
- 
-  render() {
-    return (
-      <Router>
-        <div>
+function App() {
+  return (
+    <Router>
+      <Wrapper>
+      <div>
+        <StoreProvider>
           <NavBar />
           <Header />
-          <Wrapper> 
-            <Route exact path="/" component={SignupFrom} />
-  
-            {/* <Route exact path="/" component={Entry} />
-            <Route exact path="/entries" component={Entry} />
-            <Route exact path="/habits" component={Habits} /> */} 
-           <Footer />
-          </Wrapper>
-        </div>
-     </Router>
-    )
-  };
-};
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/home" component={Homepage} />
+            {/* <Route exact path="/favorites" component={FavoritesList} />
+            <Route exact path="/posts/:id" component={Detail} /> */}
+          </Switch>
+          <Footer />
+        </StoreProvider>
+      </div>
+      </Wrapper>
+    </Router>
+  );
+}
 
 export default App;
+
