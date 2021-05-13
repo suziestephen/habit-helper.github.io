0eab2a1f4684137307e6f9559b9efcbf69246107
suziestephen <suzietstephen@gmail.com>
2021-05-13 15:22:32 +1000

    login form start

--- client/src/components/Login/LoginForm.js
+++ client/src/components/Login/LoginForm.js
@@ -1,26 +1,72 @@
-import React from "react";
-import "./loginform.css";
 
-const LoginForm = () => {
-  const handleSubmit = e => {
-    e.preventDefault();
+
+
+      
+      
+
+import React, { Component } from "react";
+import API from '../../utils/API';
+import { Form, Button } from "react-bootstrap";
+// import "./reading.css"
+
+
+
+class LoginForm extends Component {
+
+  state = {
+      email: "",
+      password: "",
+  }
+
+  change = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   };
 
+  onSubmit = e => {
+    e.preventDefault();
+    API.getUser({
+      email: this.state.email,
+      password: this.state.password,
+    });
+  };
 
-        return (
-          <form className="form-group mt-5 mb-5" onSubmit={handleSubmit}>
-          <input className="form-control mb-5" required placeholder="Email" />
-          <textarea className="form-control mb-5" required placeholder="Password" />
-          <button className="btn btn-success mt-3 mb-5" type="submit">
-            Login
-          </button>
-        </form>
-  
-        );
-      }
+  render() {
+    return (
+      <div className="signup-container">
+        <h2>Login to view the site</h2>
+      <br />
+        <Form className="signup-form">
+          <label>
+              <input 
+                name="email"
+                placeholder='Your Email' 
+                value={this.state.email} 
+                onChange={e => this.change(e)}
+              />
+            </label>
+          <br />
+          <label>
+              <input 
+                name="password"
+                placeholder='Your Password' 
+                value={this.state.password} 
+                onChange={e => this.change(e)}
+              />
+            </label>
+        </Form>
+      </div>
+      );
+    }
 
-
-      
-      
+  }
 
 export default LoginForm;
+
+
+
+
+
+
+
