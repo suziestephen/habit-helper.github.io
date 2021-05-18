cfbeb9a1447df0579d546afecb833d4a47f195ff
suziestephen <suzietstephen@gmail.com>
2021-05-18 13:07:44 +1000

    code tidyup

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,69 +9,40 @@ const axiosInstance = axios.create({
 
 export default {
 
-
+//GRATITUDE
   // // Gets all gratitude entries
   getGratitude: function() {
     return axios.get(site + "/api/entries/gratitude");
   },
 
-  deleteGratitude: function(id) {
-    return axios.delete(site + "/api/entries/gratitude" + id);
-  },
   // Saves a gratitude entries to the database
   saveGratitude: function(gratitudeData) {
     return axios.post(site + "/api/entries/gratitude", gratitudeData);
   },
 
-
+//EXERCISE
   // Gets all exercise entries
   getExercise: function() {
     return axios.get(site + "/api/entries/exercise");
   },
 
-  deleteExercise: function(id) {
-    return axios.delete(site + "/api/entries/exercise/" + id);
-  },
   // Saves a exercise entries to the database
   saveExercise: function(exerciseData) {
     return axios.post(site + "/api/entries/exercise", exerciseData);
   },
 
+  //READING
     // Gets all reading entries
     getReading: function() {
       return axios.get(site + "/api/entries/reading");
     },
 
-
-     // Gets the reading entries with the given id
-     updateReading: function(id) {
-      return axios.put(site + "/api/entries/reading/"+ id);
-    },
-
-    // Deletes the reading entries with the given id
-    deleteReading: function(id) {
-      return axios.delete(site + "/api/entries/reading/" + id);
-    },
     // Saves a reading entries to the database
     saveReading: function(readingData) {
       return axios.post(site + "/api/entries/reading", readingData);
     },
 
-
-
-    //LOGIN AND SIGNUP
-    // saveSignup: function(signupData) {
-    // return axios.post("/api/user/signup", signupData);
-    //  },
-
-    // login: function(userData) {
-    // return axios.post("/api/user/login", userData);
-    // },
-
-    // getUser: function() {
-    //   return axios.get("/api/user/login");
-    // },
-
+  //USER
     userSignup: function (userData) {
       return axiosInstance({
         method: 'post',
@@ -112,3 +83,14 @@ export default {
     
 
 };
+
+
+    //  // Gets the reading entries with the given id
+    //  updateReading: function(id) {
+    //   return axios.put(site + "/api/entries/reading/"+ id);
+    // },
+
+    // // Deletes the reading entries with the given id
+    // deleteReading: function(id) {
+    //   return axios.delete(site + "/api/entries/reading/" + id);
+    // },
